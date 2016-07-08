9108ce61688b92ffff3e3909d6b79a761ca16691
chetan <chetanbarawkar@gmail.com>
2016-07-08 12:45:20 +0530

    testapp commit

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ActiveRecord::Base
+
+  
+
+  validates :content, :presence => true, :length => { :minimum => 20 }
+  validates :title, :presence => true
+  validates :name, :presence => true
+
+end
